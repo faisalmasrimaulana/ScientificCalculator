36f3af101d5db8a92e7f30932b0a1a6895134a0a
faisalmasrimaulana <faisalmasrimaulana@gmail.com>
2025-03-03 16:59:13 +0700

    membuat form dan menambahkan icon

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ScientificCalculator
 {
-    partial class Form1
+    partial class scientificCalculatorForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(scientificCalculatorForm));
+            this.SuspendLayout();
+            // 
+            // scientificCalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1078, 544);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "scientificCalculatorForm";
+            this.Text = "Scientific Calculator";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
